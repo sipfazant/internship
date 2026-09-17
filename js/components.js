5f76da61f719f4e65a46b49da08e6b70fb067141
Eliaspro10 <154473909+Eliaspro10@users.noreply.github.com>
2026-09-17 20:13:22 +0200

    Navbar fixed

--- js/components.js
+++ js/components.js
@@ -4,6 +4,21 @@ fetch("components/navbar.html")
         document.getElementById("navbar").innerHTML = data;
     });
 
+let lastScrollY = window.scrollY;
+const navbar = document.querySelector('#navbar');
+
+window.addEventListener('scroll', () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY <= 0 || currentScrollY < lastScrollY) {
+        navbar.classList.remove('nav-hidden');
+    } else {
+        navbar.classList.add('nav-hidden');
+    }
+
+    lastScrollY = currentScrollY;
+});
+
 fetch("components/footer.html")
     .then(response => response.text())
     .then(data => {
